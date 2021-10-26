f6183755d43ae0dfe8263a4f01cf74d558399db7
sgwulffraat <stijn.wulffraat@gmail.com>
2021-10-26 15:30:48 +0200

    Assignment 1 added airfoil importer

---
+++ Aircraft Aerodynamics/Airfoilgeometrycalculator.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math as m
+
+def airfoil_geometery(Xdata, Ydata, AoA):
+    # Number of panels
+    N_pan= len(Xdata) - 1
+
+    # Check for direction of points
+    edge = np.zeros(N_pan)
+    for i in range(N_pan):
+        edge[i] = (Xdata[i+1]-Xdata[i])*(Ydata[i+1]+Ydata[i])
+    sumEdge = np.sum(edge)
+
+    # If panels are CCW, flip them (don't if CW)
+    if (sumEdge < 0):
+        Xdata = np.flipud(Xdata)
+        Ydata = np.flipud(Ydata)
+    elif (sumEdge > 0):
+
+    #COMPUTE GEOMETRIC VARIABLES
+
+    # Initialize variables
+    XC = np.zeros(N_pan)
+    YC = np.zeros(N_pan)
+    S = np.zeros(N_pan)
+    phi = np.zeros(N_pan)
+
+    # Find geometric quantities of the airfoil
+    for i in range(N_pan):
+        XC[i] = 0.5 * (Xdata[i] + Xdata[i + 1])
+        YC[i] = 0.5 * (Ydata[i] + Ydata[i + 1])
+        dx = Xdata[i + 1] - Xdata[i]
+        dy = Ydata[i + 1] - Ydata[i]
+        S[i] = (dx ** 2 + dy ** 2) ** 0.5
+        phi[i] = m.atan2(dy, dx)
+        if (phi[i] < 0):
+            phi[i] = phi[i] + 2 * np.pi
+
+    # Compute angle of panel normal w.r.t. horizontal and include AoA
+    AoAr = AoA*np.pi/180
+    delta = phi + (np.pi / 2)  # Panel normal angle [rad]
+    beta = delta - (AoAr * (np.pi / 180))  # Angle between freestream and panel normal [rad]
+
+    return XC, YC, S, delta, beta
--- Aircraft Aerodynamics/Assignment 1.py
+++ Aircraft Aerodynamics/Assignment 1.py
@@ -1,16 +1,18 @@
 from Airfoilselector import NACAcalculator
-from math import *
+import math as m
 import numpy as np
 import matplotlib.pyplot as plt
 from Airfoilimporter import import_airfoil
+from Airfoilgeometrycalculator import airfoil_geometery
 
-# Airfoil thicknesses
+# Flight conditions parameters
+AoA = 0
+M = 0.8
+
+# Initiate airfoil selector
 Naca = NACAcalculator()
-N1 = Naca[0]
-N2 = Naca[1]
 
-
-#Importing Airfoil Geometry
+# Importing Airfoil Geometry
 AF1 = import_airfoil(Naca[2])
 AF2 = import_airfoil(Naca[3])
 X1 = AF1[0]
@@ -18,11 +20,21 @@ Y1 = AF1[1]
 X2 = AF2[0]
 Y2 = AF2[1]
 
-x1,y1 = AF1
-x2,y2 = AF2
-# plt.figure()
-# plt.plot(X1,Y1)
-# plt.show()
-plt.figure()
-plt.plot(x1,y1)
+#Geometery parameters
+Geom1 = airfoil_geometery(X1, Y1, AoA)
+Geom2 = airfoil_geometery(X2, Y2, AoA)
+
+# %% PLOTTING
+
+# Plot the paneled geometry
+fig = plt.figure(1)
+plt.plot(X1, Y1, color='orange', marker='o', markersize = 3, label = Naca[2])
+plt.plot(X2, Y2, color='blue', marker='o', markersize = 3, label = Naca[3])
+plt.plot(0, 0, color='w', label = "AoA = "+str(AoA)+"\N{DEGREE SIGN}")
+plt.plot(0, 0, color='w', label = "M = "+str(M))
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.title('Panel Geometry')
+plt.axis('equal')
+plt.legend()
 plt.show()
