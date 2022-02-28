08ec3292c780c1f1c28e6c7fd59c823c8c82cb52
sgwulffraat <stijn.wulffraat@gmail.com>
2022-02-28 17:02:16 +0100

    new course

---
+++ Aircraft Emissions/Assignment 1.py
@@ -0,0 +1,34 @@
+from math import *
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("NL_trajectories_2021.txt", sep=',', header=0)
+#data.columns = ["time", "icao24","lat","lon","velocity","heading","vertrate","callsign","onground","alert","spi","squawk","baroaltitude","geoaltitude","lastposupdate","lastcontact"]
+print(data[['callsign', 'onground']][0:10])
+world_map = gpd.read_file(gpd.datasets.getpath('naturalearth_lowres'))
+
+grouped = data.groupby(data.callsign)
+flight1 = grouped.get_group("PGT27W")
+flight2 = grouped.get_group("AFR96ZN")
+flight3 = grouped.get_group("KLM862")
+flight4 = grouped.get_group("AFR15AH")
+flight5 = grouped.get_group("PGT53X")
+flight6 = grouped.get_group("DLH908")
+flight7 = grouped.get_group("GEC8201")
+flight8 = grouped.get_group("KLM685")
+flight9 = grouped.get_group("KLM862")
+
+plt.plot(flight1.lon, flight1.lat)
+plt.plot(flight2.lon, flight2.lat)
+plt.plot(flight3.lon, flight3.lat)
+plt.plot(flight4.lon, flight4.lat)
+plt.plot(flight5.lon, flight5.lat)
+plt.plot(flight6.lon, flight6.lat)
+plt.plot(flight7.lon, flight7.lat)
+plt.plot(flight8.lon, flight8.lat)
+plt.plot(flight9.lon, flight9.lat)
+plt.show()
+
