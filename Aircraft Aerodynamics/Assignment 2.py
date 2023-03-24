8db002f6fa63ad5ab7c986a0cee8d0890fd23d54
sgwulffraat <sgwulffraat@student.tudelft.nl>
2023-03-24 16:08:54 +0100

    Final assignment 2

--- Aircraft Aerodynamics/Assignment 2.py
+++ Aircraft Aerodynamics/Assignment 2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from XFOIL2 import xfoil
-from XFOIL_ID import xfoil_id
+from XFOIL_ID import xfoil_id, xfoil_trs
 
 # %% INPUTS - PART I #
 NACA = "2614"
@@ -39,67 +39,109 @@ for R in [2E05, 4E05, 6E05]:
     cp_re, x_nouse = xfoil_id(2614, R, 12, 0.7)
     cp_list.append(cp_re)
 
+cd_list = []
+trans = np.arange(0.4, 0.75, 0.05)
+for xtr in trans:
+    cd, cp, x = xfoil_trs(2614, 2E05, 12, 0.7, xtr)
+    cd_list.append(cd)
+
+cd_init, cp_init, x_init = xfoil_trs(2614, 2E05, 12, 0.7, 1)
+cd_new, cp_new, x_new = xfoil_trs(2614, 2E05, 12, 0.7, 0.65)
+
+
+
+#Original xtr = 0.6969 and  CD =  0.01120
+print()
+print("Forced transition at: ", trans)
+print("Respective drag: ", cd_list)
 # %% PLOTTING %% #
+#Plotting flag
+flag = [
+    1,      # Set to 1 to show Lift polar plot
+    1,      # Set to 1 to show Drag polar plot
+    1,      # Set to 1 to show Transition point plot
+    1,      # Set to 1 to show Skin friction plot
+    1,      # Set to 1 to show Optimized cp plot
+    1,      # Set to 1 to show Cp for different Re plot
+    1,      # Set to 1 to show Cp for fixed transition plot
+]
 
 # Plotting lift polar
-fig = plt.figure()
-plt.plot(alpha, cl_polar, color='tab:blue', marker='o', markersize=2)
-plt.xlabel(chr(945))
-plt.ylabel('Cl')
-plt.title("Lift Polar")
-plt.grid()
-plt.show()
+if flag[0] == 1:
+    fig1 = plt.figure()
+    plt.plot(alpha, cl_polar, color='tab:blue', marker='o', markersize=2)
+    plt.xlabel(chr(945))
+    plt.ylabel('Cl')
+    plt.grid()
+    plt.show()
 
 # Plotting drag polar
-fig2 = plt.figure()
-plt.plot(cd_polar, cl_polar, color='tab:blue', marker='o', markersize=2)
-plt.xlabel('Cd')
-plt.ylabel('Cl')
-plt.grid()
-plt.show()
+if flag[1] == 1:
+    fig2 = plt.figure()
+    plt.plot(cd_polar, cl_polar, color='tab:blue', marker='o', markersize=2)
+    plt.xlabel('Cd')
+    plt.ylabel('Cl')
+    plt.grid()
+    plt.show()
 
 # Plotting location of transition point
-fig3 = plt.figure()
-plt.plot(bot_xtr_pol, alpha, label='Bottom', color='tab:blue', marker='o', markersize=2)
-plt.plot(top_xtr_pol, alpha, label='Top', color='tab:orange', marker='o', markersize=2)
-plt.xlabel('Transition point (x/c)')
-plt.ylabel(chr(945))
-plt.legend()
-plt.grid()
-plt.show()
+if flag[2] == 1:
+    fig3 = plt.figure()
+    plt.plot(bot_xtr_pol, alpha, label='Bottom', color='tab:blue', marker='o', markersize=2)
+    plt.plot(top_xtr_pol, alpha, label='Top', color='tab:orange', marker='o', markersize=2)
+    plt.xlabel('Transition point (x/c)')
+    plt.ylabel(chr(945))
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 # Plotting skin friction
-fig3 = plt.figure()
-plt.plot(x_a0, cf_a0, label='a = 0', color='tab:blue')
-plt.plot(x_a4, cf_a4, label='a = 4', color='tab:orange')
-plt.xlabel('Chordwise position (x/c)')
-plt.ylabel('Skin friction (Cf)')
-plt.legend()
-plt.grid()
-plt.show()
+if flag[3] == 1:
+    fig4 = plt.figure()
+    plt.plot(x_a0, cf_a0, label=r'α = 0', color='tab:blue')
+    plt.plot(x_a4, cf_a4, label=r'α = 4', color='tab:orange')
+    plt.xlabel('Chordwise position (x/c)')
+    plt.ylabel('Skin friction (Cf)')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 # Plotting cp distributions
-fig4 = plt.figure()
-plt.plot(x_org, cp_org, label='Original', color='tab:blue')
-plt.plot(x_opt, cp_opt, label='Optimized', color='tab:orange')
-plt.gca().invert_yaxis()
-plt.xlabel('Chordwise position (x/c)')
-plt.ylabel('Pressure coefficient (Cp)')
-plt.legend()
-plt.grid()
-plt.show()
+if flag[4] == 1:
+    fig5 = plt.figure()
+    plt.plot(x_org, cp_org, label='Original', color='tab:blue')
+    plt.plot(x_opt, cp_opt, label='Optimized', color='tab:orange')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Chordwise position (x/c)')
+    plt.ylabel('Pressure coefficient (Cp)')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 # Plotting cp distributions for different Re numbers
-fig5 = plt.figure()
-plt.plot(x_org, cp_list[0], label=r'$Re = 2,0*10^5$', color='tab:blue')
-plt.plot(x_org, cp_list[1], label=r'$Re = 4,0*10^5$', color='tab:orange')
-plt.plot(x_org, cp_list[2], label=r'$Re = 6,0*10^5$', color='tab:green')
-plt.gca().invert_yaxis()
-plt.xlabel('Chordwise position (x/c)')
-plt.ylabel('Pressure coefficient (Cp)')
-plt.legend()
-plt.grid()
-plt.show()
+if flag[5] == 1:
+    fig6 = plt.figure()
+    plt.plot(x_org, cp_list[0], label=r'$Re = 2,0*10^5$', color='tab:blue')
+    plt.plot(x_org, cp_list[1], label=r'$Re = 4,0*10^5$', color='tab:orange')
+    plt.plot(x_org, cp_list[2], label=r'$Re = 6,0*10^5$', color='tab:green')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Chordwise position (x/c)')
+    plt.ylabel('Pressure coefficient (Cp)')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
+
+# Plotting cp distributions for different Re numbers
+if flag[6] == 1:
+    fig7 = plt.figure()
+    plt.plot(x_init, cp_init, label='Free transition', color='tab:blue')
+    plt.plot(x_new, cp_new, label='Fixed transition at x/c = 0.65', color='tab:orange')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Chordwise position (x/c)')
+    plt.ylabel('Pressure coefficient (Cp)')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 
--- Aircraft Aerodynamics/XFOIL_ID.py
+++ Aircraft Aerodynamics/XFOIL_ID.py
@@ -10,7 +10,7 @@ def xfoil_id(filename, re, n_crit, cl):
     saveFlnmAF = 'Save_Airfoil.txt'
     saveFlnmCp = 'Save_Cp.txt'
     xfoilFlnm = 'xfoil_input.txt'
-    iter = "20"
+    iter = "30"
     ncrit = str(n_crit)
 
     # Delete files if they exist
@@ -65,8 +65,81 @@ def xfoil_id(filename, re, n_crit, cl):
     X = dataBuffer[:, 0]
     Cp = dataBuffer[:, 2]
 
-
-
     return Cp, X
+
+
+def xfoil_trs(naca, re, n_crit, cl, trs_point):
+    # %% CREATE LOADING FILE
+    # Knowns
+    CL = cl
+    saveFlnmAF = 'Save_Airfoil.txt'
+    saveFlnmCp = 'Save_Cp.txt'
+    xfoilFlnm = 'xfoil_input.txt'
+    saveFlnmPol = 'Save_Pol.txt'
+    iter = "30"
+    ncrit = str(n_crit)
+
+    # Delete files if they exist
+    #if os.path.exists(saveFlnmAF):
+        #os.remove(saveFlnmAF)
+
+    # Create the airfoil
+    fid = open(xfoilFlnm, "w")
+    fid.write("NACA " + str(naca) + "\n")
+    fid.write("PSAV " + saveFlnmAF + "\n")
+    if os.path.exists(saveFlnmAF):
+        fid.write("y \n")
+    fid.write("OPER\n")
+    fid.write("iter\n")
+    fid.write(iter + "\n")
+    fid.write("visc\n")
+    fid.write(str(re) + "\n")
+    fid.write("VPAR\n")
+    if n_crit != 9:
+        fid.write("n " + ncrit + "\n")
+    fid.write("xtr" + "\n")
+    fid.write(str(trs_point) + "\n")
+    fid.write("1 \n \n")
+    fid.write("Pacc 1 \n")
+    fid.write("\n\n")
+    fid.write("cl \n")
+    fid.write(str(CL)+ "\n")
+    fid.write("CPWR " + saveFlnmCp + "\n")
+    if os.path.exists(saveFlnmCp):
+        fid.write("y \n")
+    fid.write("PWRT\n")
+    fid.write(saveFlnmPol + "\n")
+    if os.path.exists(saveFlnmPol):
+        fid.write("y \n")
+    fid.close()
+
+    # Run the XFoil calling command
+    os.system("xfoil.exe < xfoil_input.txt")
+
+    # Delete file after running
+    #if os.path.exists(xfoilFlnm):
+       # os.remove(xfoilFlnm)
+
+    # %% READ DATA FILE: PRESSURE COEFFICIENT
+
+    # Load the data from the text file
+    fin = open(saveFlnmCp, 'r+')
+    fout = open("Save_Cpcheck.txt", 'w+')
+    lines = fin.readlines()
+    for line in lines:
+        fout.write(line.replace('-', ' -'))
+    fin.close()
+    fout.close()
+    dataBuffer = np.loadtxt('Save_Cpcheck.txt', skiprows=3)
+
+    # Polar
+    dataBuffer2 = np.loadtxt(saveFlnmPol, skiprows=12)
+    Cd = dataBuffer2[2]
+
+    # Extract data from the loaded dataBuffer array
+    X = dataBuffer[:, 0]
+    Cp = dataBuffer[:, 2]
+
+    return Cd, Cp, X
 
 
